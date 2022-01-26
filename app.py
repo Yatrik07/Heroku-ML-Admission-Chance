38661783fd647cbaa8d581aa21ffc4ed325f856f
Yatrik07 <86146170+Yatrik07@users.noreply.github.com>
2022-01-26 16:38:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,30 +14,29 @@ def home():
 @cross_origin()
 def predict():
     if request.method=='POST':
-        try:
-            gre_score = float(request.form['gre_score'])
-            toefl_score = float(request.form['toefl_score'])
-            university_rating = float(request.form['university_rating'])
-            sop = float(request.form['sop'])
-            lor = float(request.form['lor'])
-            cgpa = float(request.form['cgpa'])
-            research = request.form['research']
+        
+        gre_score = float(request.form['gre_score'])
+        toefl_score = float(request.form['toefl_score'])
+        university_rating = float(request.form['university_rating'])
+        sop = float(request.form['sop'])
+        lor = float(request.form['lor'])
+        cgpa = float(request.form['cgpa'])
+        research = request.form['research']
 
-            if research=="yes":
+        if research=="yes":
                 research=1
-            else:
+        else:
                 research=0
 
-            model = joblib.load('Final_Model.pickle')
-            scaler = joblib.load('scaler_object.pickle')
+        model = joblib.load('Final_Model.pickle')
+        scaler = joblib.load('scaler_object.pickle')
 
-            prediction = model.predict(scaler.transform(np.array([[gre_score , toefl_score , university_rating , sop , lor , cgpa , research]])))
-            prediction1 = round(prediction[0],2) * 100
+        prediction = model.predict(scaler.transform(np.array([[gre_score , toefl_score , university_rating , sop , lor , cgpa , research]])))
+        prediction1 = round(prediction[0],2) * 100
 
-            return render_template('results.html' , prediction = str(prediction1)+'%')
+        return render_template('results.html' , prediction = str(prediction1)+'%')
 
-        except ValueError:
-            return render_template('ValueError.html')
+        
         except :
             return render_template('Error.html')		
 		
